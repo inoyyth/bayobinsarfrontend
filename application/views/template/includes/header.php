87bb471cb8f36ef3df8109fdd5f250ab50578ef8
supriyadin <supri170845@gmail.com>
2018-10-09 23:25:26 +0700

    update css file use cache datetime

--- application/views/template/includes/header.php
+++ application/views/template/includes/header.php
@@ -8,17 +8,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="img/icon.png">
 		<link href="https://fonts.googleapis.com/css?family=Fira+Sans:300i,400,400i,500,600,700,800,900" rel="stylesheet">
-		<link rel="stylesheet" href="<?php echo base_url('assets/themes/css/bootstrap.min.css');?>">
-		<link rel="stylesheet" href="<?php echo base_url('assets/themes/css/slicknav.min.css');?>">
-		<link rel="stylesheet" href="<?php echo base_url('assets/themes/css/bootstrap-theme.min.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/font-awesome.min.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/owl.carousel.min.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/owl.theme.default.min.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/animate.min.css');?>">
-        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/magnific-popup.css');?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/themes/css/bootstrap.min.css?'.date('YmdHis'));?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/themes/css/slicknav.min.css?'.date('YmdHis'));?>">
+		<link rel="stylesheet" href="<?php echo base_url('assets/themes/css/bootstrap-theme.min.css?'.date('YmdHis'));?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/font-awesome.min.css?'.date('YmdHis'));?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/owl.carousel.min.css?'.date('YmdHis'));?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/owl.theme.default.min.css?'.date('YmdHis'));?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/animate.min.css?'.date('YmdHis'));?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/magnific-popup.css?'.date('YmdHis'));?>">
 		
 		<!-- Xman CSS -->
-        <link rel="stylesheet" href="<?php echo base_url('assets/themes/ss/normalize.css');?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/themes/ss/normalize.css?'.date('YmdHis'));?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/themes/style.css?'.date('YmdHis'));?>">
         <link rel="stylesheet" href="<?php echo base_url('assets/themes/css/responsive.css?'.date('YmdHis'));?>">
         <script src="<?php echo base_url('assets/themes/js/jquery.min.js');?>">  </script>
